cef673e632d7190eed5a3bcedf5e45be48db3550
kyawthihalin <kyawthihapersonal@gmail.com>
2022-11-03 13:27:53 +0630

    [add] user_game_type_table

---
+++ database/migrations/2022_11_03_064227_create_game_type_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('game_type_user', function (Blueprint $table) {
+            $table->id();
+            $table->uuid("user_id")->nullable()->index();
+            $table->foreign("user_id")->references("id")->on("users");
+            $table->unsignedBigInteger("game_type_id")->nullable()->index();
+            $table->foreign("game_type_id")->references("id")->on("game_types");
+            $table->double('amount')->default(0.00);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('game_type_user');
+    }
+};
