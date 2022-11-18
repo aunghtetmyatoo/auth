9abae2a5971364c94ebc1ffa3f4024733ae935c3
wannaminpaing <paingwanna@gmail.com>
2022-11-18 09:20:11 +0630

    add friend function

--- app/Http/Controllers/FriendController.php
+++ app/Http/Controllers/FriendController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use App\Constants\Status;
 use App\Http\Requests\Api\Friend\FriendAddedListRequest;
 use App\Http\Requests\Api\Friend\FriendAddRequest;
+use App\Http\Requests\Api\Friend\FriendCancelRequest;
 use App\Http\Requests\Api\Friend\FriendConfirmRequest;
 use App\Http\Requests\Api\Friend\FriendIndexRequest;
 use App\Http\Resources\Api\Friend\FriendCollection;
 use App\Models\Friend;
 use App\Traits\Auth\ApiResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class FriendController extends Controller
 {
@@ -18,7 +20,7 @@ class FriendController extends Controller
 
     public function index(FriendIndexRequest $request)
     {
-        $friend_list = Friend::where('user_id',$request->user_id)->where('confirm_status',Status::RECEIVED_FRIEND)->get();
+        $friend_list = Friend::where('user_id',$request->user_id)->where('confirm_status',Status::CONFIRMED_FRIEND)->get();
 
         return new FriendCollection($friend_list);
 
@@ -41,7 +43,7 @@ class FriendController extends Controller
             [
                 'user_id' => $request->friend_id,
                 'friend_id' => $request->user_id,
-                'confirm_status' => Status::CONFIRMED_FRIEND,
+                'confirm_status' => Status::RECEIVED_FRIEND,
             ]
         ]);
 
@@ -50,12 +52,28 @@ class FriendController extends Controller
 
     public function confirmFriend(FriendConfirmRequest $request)
     {
-        $confirm_friend =  Friend::where('user_id',$request->friend_id)->where('friend_id',$request->user_id)->firstOrFail();
+        DB::beginTransaction();
+        try {
+            $confirm_friend_one =  Friend::where('user_id',$request->user_id)->where('friend_id',$request->friend_id)->firstOrFail();
+            $confirm_friend_one->update([
+                'confirm_status' => Status::CONFIRMED_FRIEND,
+                ]);
 
-        $confirm_friend->update([
-            'confirm_status' => Status::RECEIVED_FRIEND,
-        ]);
-        return $this->responseSucceed(message: "Confirm Friend Successfully");
+            $confirm_friend_two =  Friend::where('user_id',$request->friend_id)->where('friend_id',$request->user_id)->firstOrFail();
+            $confirm_friend_two->update([
+                'confirm_status' => Status::CONFIRMED_FRIEND,
+                ]);
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollback();
+            return $e->getMessage();
+        }
+
+        return $this->responseSucceed(message: "Confirmed Friend Successfully");
     }
 
+    // public function cancelFriend(FriendCancelRequest $request)
+    // {
+    //     return "canceel";
+    // }
 }
