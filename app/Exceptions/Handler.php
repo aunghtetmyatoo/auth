3b322a804cadaa079b0034c65b2f945561b35f1a
aunghtetmyatoo <aunghtetmyatoo888@gmail.com>
2023-01-13 11:35:32 +0630

    update'

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -26,7 +26,7 @@ class Handler extends ExceptionHandler
      * @var array<int, class-string<\Throwable>>
      */
     protected $dontReport = [
-        \League\OAuth2\Server\Exception\OAuthServerException::class
+
     ];
 
     /**
@@ -47,8 +47,13 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->reportable(function (Throwable $exception) {
+            // Kill reporting if this is an "access denied" (code 9) OAuthServerException.
+            if ($exception instanceof \League\OAuth2\Server\Exception\OAuthServerException && $exception->getCode() == 9) {
+                return;
+            }
+
+            parent::report($exception);
         });
     }
 
