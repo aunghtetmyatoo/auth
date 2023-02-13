92822bf4d7d4b81da638759949e87d951bba438c
Aye Myat Myat Mon <ayemyatmyatmon@AyeMyatMyatMon-MacBook-Pro.local>
2023-02-13 09:13:35 +0630

    Updating Withdraw Request

--- app/Http/Requests/Api/WithdrawRequest/ThaibahtCreaterequest.php
+++ app/Http/Requests/Api/WithdrawRequest/ThaibahtCreaterequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Api\WithdrawRequest;
 
 use App\Models\WithdrawChannel;
+use App\Actions\DevelopmentValidator;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ThaibahtCreaterequest extends FormRequest
@@ -25,7 +26,12 @@ class ThaibahtCreaterequest extends FormRequest
     public function rules()
     {
         $channel = WithdrawChannel::where('name','Thai Baht')->first();
-
+        (new DevelopmentValidator())->handle([
+            // 'device_id' => ['required', 'string', 'max:32'],
+            // 'session_id' => ['required', 'string', 'max:60'],
+            'passcode' => ['required', 'string'],
+            // 'amount' => ['required', 'numeric', 'integer'],
+        ]);
         return [
             'payee' => ['required', 'string', 'max:255'],
             'account_number' => ['required', 'string', 'max:255'],
