f0095462cbd4f4238fd49cb434e8a21cb99aacb0
aunghtetmyatoo <aunghtetmyatoo888@gmail.com>
2023-03-06 16:10:55 +0630

    update

--- app/Actions/HandleEndpoint.php
+++ app/Actions/HandleEndpoint.php
@@ -11,22 +11,16 @@ class HandleEndpoint
     public function handle(string $server_path, array $request)
     {
 
-        $response = Http::post( $server_path, str_starts_with($server_path,'https://game-socket.com') ? array_merge($request, [
+        $response = Http::post($server_path, str_starts_with($server_path, 'https://game-socket.com') ? array_merge($request, [
             'client_id' => env('SOCKET_CLIENT_ID', 'SOID00BD48FBUFTHN67G4D'),
             'client_secret' => env('SOCKET_CLIENT_SECRET', 'SOSC44zmcA8nGvEGBFmvLj8FpQlHgIfxIqt0lBbgRLnPwNheWQyMD')
         ]) : $request);
 
-        return json_decode($response, true);
-        // return $response;
-        // $data=json_decode($response);
-        //     return (new DataKey())->encrypt([
-        //         $data
-        //     ]);
-        // $response = Http::post(config("api.server.$server_name.end_point") . config("api.server.$server_name.$prefix.prefix") . config("api.server.$server_name.$prefix.$route_name"), $server_name === "real_time" ? array_merge($request, [
-        //     'client_id' => env('SOCKET_CLIENT_ID', 'SOID00BD48FBUFTHN67G4D'),
-        //     'client_secret' => env('SOCKET_CLIENT_SECRET', 'SOSC44zmcA8nGvEGBFmvLj8FpQlHgIfxIqt0lBbgRLnPwNheWQyMD')
-        // ]) : $request);
-
-        // return json_decode($response, true);
+        return response()->json([
+            (new DataKey())->encrypt(
+                json_decode($response, true),
+                auth()->user()->secret_key
+            )
+        ]);
     }
 }
