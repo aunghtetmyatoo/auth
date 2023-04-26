84a8cbf48bbba31d95810af428c932fe7f82546c
WannaMinPaing <paingwanna@gmail.com>
2023-04-26 16:54:38 +0630

    add refill migration

---
+++ database/migrations/2023_04_26_103632_create_it_head_to_super_admin_fill_logs.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('it_head_to_super_admin_fill_logs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('from_user_id')->nullable();
+            $table->unsignedBigInteger('to_user_id')->nullable();
+            $table->double('amount')->default(0);
+            $table->text('remark')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('it_head_to_super_admin_fill_logs');
+    }
+};
---
+++ database/migrations/2023_04_26_152830_create_finance_manager_to_finance_fill_logs.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('finance_manager_to_finance_fill_logs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('from_user_id')->nullable();
+            $table->unsignedBigInteger('to_user_id')->nullable();
+            $table->double('amount')->default(0);
+            $table->text('remark')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('finance_manager_to_finance_fill_logs');
+    }
+};
